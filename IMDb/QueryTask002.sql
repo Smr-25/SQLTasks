8924af2db20b2dc31df05c341ab3b76ce64d6c70
Smr-25 <samiraa@code.edu.az>
2025-10-22 10:34:22 +0400

    22.10.25

--- IMDb/QueryTask002.sql
+++ IMDb/QueryTask002.sql
@@ -1,33 +1,141 @@
-CREATE DATABASE IMDB
-USE IMDB
+CREATE DATABASE IMDb
+USE IMDb
 
-CREATE TABLE Movies 
+CREATE TABLE Directors
 (
-Title NVARCHAR(50),
-DirectoryName NVARCHAR(50),
-Genre NVARCHAR(50),
-Rating DECIMAL(3,1)
+    DirectorId       INT PRIMARY KEY IDENTITY,
+    DirectorFullName NVARCHAR(50) NOT NULL,
+    Country          NVARCHAR(50)
 )
 
-CREATE TABLE Directors 
+CREATE TABLE Movies
 (
-FullName NVARCHAR(50),
-AGE INT,
-Genre NVARCHAR(50),
-Rating DECIMAL(3,1)
+    MovieId        INT PRIMARY KEY IDENTITY,
+    Title          NVARCHAR(50) NOT NULL,
+    Raiting        DECIMAL(3, 1),
+    ReleaseYear    INT,
+    RuntimeMinutes INT,
+    DirectorId     INT,
+    CONSTRAINT FK_Movies_Directors FOREIGN KEY (DirectorId) REFERENCES Directors (DirectorId)
 )
 
-CREATE TABLE Actors(
-Title NVARCHAR(50),
-DirectoryName NVARCHAR(50),
-Genre NVARCHAR(50),
-Rating DECIMAL(3,1)
+CREATE TABLE Genres
+(
+    GenreId   INT PRIMARY KEY IDENTITY,
+    GenreName NVARCHAR(50) NOT NULL
 )
 
-CREATE TABLE Genres(
-Title NVARCHAR(50),
-DirectoryName NVARCHAR(50),
-Genre NVARCHAR(50),
-Rating DECIMAL(3,1)
+CREATE TABLE Actors
+(
+    ActorId       INT PRIMARY KEY IDENTITY,
+    ActorFullName NVARCHAR(50) NOT NULL,
+    BirthYear     INT
 )
+
+
+CREATE TABLE MoviesGenres
+(
+    Id      INT PRIMARY KEY IDENTITY,
+    MovieId INT FOREIGN KEY REFERENCES Movies (MovieId),
+    GenreId INT FOREIGN KEY REFERENCES Genres (GenreId)
+)
+
+
+CREATE TABLE MoviesActors
+(
+    Id      INT PRIMARY KEY IDENTITY,
+    MovieId INT FOREIGN KEY REFERENCES Movies (MovieId),
+    ActorId INT FOREIGN KEY REFERENCES Actors (ActorId)
+)
+
+
+INSERT INTO Movies (Title, Raiting, ReleaseYear, RuntimeMinutes)
+VALUES ('Inception', 8.8, 2010, 148),
+       ('The Dark Knight', 9.0, 2008, 152),
+       ('Interstellar', 8.6, 2014, 169),
+       ('Pulp Fiction', 8.9, 1994, 154),
+       ('The Shawshank Redemption', 9.3, 1994, 142)
+
+INSERT INTO Directors (DirectorFullName, Country)
+VALUES ('Christopher Nolan', 'UK'),
+       ('Quentin Tarantino', 'USA'),
+       ('Frank Darabont', 'USA')
+
+INSERT INTO Genres (GenreName)
+VALUES ('Action'),
+       ('Drama'),
+       ('Sci-Fi'),
+       ('Crime')
+
+INSERT INTO Actors (ActorFullName, BirthYear)
+VALUES ('Leonardo DiCaprio', 1974),
+       ('Christian Bale', 1974),
+       ('Matthew McConaughey', 1969),
+       ('John Travolta', 1954),
+       ('Morgan Freeman', 1937)
+
+
+INSERT INTO MoviesGenres (MovieId, GenreId)
+VALUES (1, 1),
+       (1, 3),
+       (2, 1),
+       (2, 4),
+       (3, 2),
+       (3, 3),
+       (4, 2),
+       (4, 4),
+       (5, 2)
+
+INSERT MoviesActors(MovieId, ActorId)
+VALUES (1, 1),
+       (2, 2),
+       (3, 3),
+       (4, 4),
+       (5, 5)
+
+UPDATE Movies
+SET DirectorId = 1
+WHERE Title IN ('Inception', 'The Dark Knight', 'Interstellar');
+UPDATE Movies
+SET DirectorId = 2
+WHERE Title = 'Pulp Fiction';
+UPDATE Movies
+SET DirectorId = 3
+WHERE Title = 'The Shawshank Redemption';
+
+--Query 1
+SELECT m.Title, m.Raiting, g.GenreName, d.DirectorFullName, a.ActorFullName
+FROM Movies m
+         JOIN MoviesGenres mg ON m.MovieId = mg.MovieId
+         JOIN Genres g ON mg.GenreId = g.GenreId
+         JOIN Directors d ON m.DirectorId = d.DirectorId
+         JOIN MoviesActors ma ON m.MovieId = ma.MovieId
+         JOIN Actors a ON ma.ActorId = a.ActorId
+WHERE m.Raiting > 6.0
+
+--Query 2
+SELECT m.Title, m.Raiting, g.GenreName
+FROM Movies m
+         JOIN MoviesGenres mg ON m.MovieId = mg.MovieId
+         JOIN Genres g ON mg.GenreId = g.GenreId
+WHERE g.GenreName LIKE '%a%'
+
+--Query 3
+SELECT m.Title, m.Raiting, m.RuntimeMinutes, g.GenreName
+FROM Movies m
+         JOIN MoviesGenres mg ON m.MovieId = mg.MovieId
+         JOIN Genres g ON mg.GenreId = g.GenreId
+WHERE LEN(m.Title) > 10
+  AND m.Title LIKE '%t'
+
+--Query 4
+SELECT m.Title, m.Raiting, g.GenreName, d.DirectorFullName, a.ActorFullName
+FROM Movies m
+         JOIN MoviesGenres mg ON m.MovieId = mg.MovieId
+         JOIN Genres g ON mg.GenreId = g.GenreId
+         JOIN Directors d ON d.DirectorId = m.DirectorId
+         JOIN MoviesActors ma ON ma.MovieId = m.MovieId
+         JOIN Actors a ON a.ActorId = ma.ActorId
+WHERE m.Raiting > (SELECT AVG(m.Raiting) FROM Movies m)
+ORDER BY m.Raiting DESC
 
---
+++ Spotify/QueryTask003.sql
@@ -0,0 +1,69 @@
+CREATE DATABASE Spotify
+USE Spotify
+
+CREATE TABLE Artists
+(
+    ArtistId       INT PRIMARY KEY IDENTITY,
+    ArtistFullName NVARCHAR(100) NOT NULL,
+    BirthYear      NVARCHAR(50)
+)
+
+CREATE TABLE Albums
+(
+    AlbumId     INT PRIMARY KEY IDENTITY,
+    Title       NVARCHAR(100) NOT NULL,
+    ReleaseYear INT,
+    ArtistId    INT,
+    CONSTRAINT FK_Albums_Artists FOREIGN KEY (ArtistId) REFERENCES Artists (ArtistId)
+)
+
+CREATE TABLE Musics
+(
+    MusicId         INT PRIMARY KEY IDENTITY,
+    Title           NVARCHAR(100) NOT NULL,
+    Popularity      INT,
+    DurationSeconds INT,
+    AlbumId         INT,
+    ArtistId        INT,
+    CONSTRAINT FK_Songs_Albums FOREIGN KEY (AlbumId) REFERENCES Albums (AlbumId),
+    CONSTRAINT FK_Songs_Artists FOREIGN KEY (ArtistId) REFERENCES Artists (ArtistId)
+)
+
+INSERT INTO Artists (ArtistFullName, BirthYear)
+VALUES ('Taylor Swift', '1989'),
+       ('Ed Sheeran', '1991'),
+       ('Adele', '1988'),
+       ('Drake', '1986'),
+       ('Beyoncé', '1981')
+
+INSERT INTO Albums (Title, ReleaseYear, ArtistId)
+VALUES ('1989', 2014, 1),
+       ('Divide', 2017, 2),
+       ('25', 2015, 3),
+       ('Scorpion', 2018, 4),
+       ('Lemonade', 2016, 5)
+
+INSERT INTO Musics (Title, Popularity, DurationSeconds, AlbumId, ArtistId)
+VALUES ('Blank Space', 95, 231, 1, 1),
+       ('Shape of You', 98, 233, 2, 2),
+       ('Hello', 97, 295, 3, 3),
+       ('Gods Plan', 96, 198, 4, 4),
+       ('Formation', 94, 220, 5, 5),
+       ('Bad Blood', 93, 211, 1, 1),
+       ('Perfect', 92, 263, 2, 2),
+       ('Someone Like You', 91, 285, 3, 3),
+       ('In My Feelings', 90, 217, 4, 4),
+       ('Sorry', 89, 200, 5, 5)
+
+
+--Query 1:
+SELECT m.Title,al.Title,ar.ArtistFullName,m.DurationSeconds FROM Musics m
+JOIN Albums al ON al.AlbumId = m.AlbumId
+JOIN Artists ar ON ar.ArtistId = m.ArtistId
+
+--Query 2:
+--SELECT al.Title,COUNT(m.MusicId) FROM Musics m
+--LEFT JOIN Albums al ON al.AlbumId = m.AlbumId
+
+
+
